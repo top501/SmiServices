eea40541e44ec733091fd66c7d2be82f832bbb5f
James A Sutherland <j@sutherland.pw>
2020-02-04 19:52:01 +0000

    Fix git mangling in shell script

--- lib/java/installDat.sh
+++ lib/java/installDat.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # Install DATs dependencies to your local repo
-mvn install:install-file -Dfile="./CTP.jar" -DgroupId="dat" -DartifactId="CTP" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn install:install-file -Dfile="./dcm4che.jar" -DgroupId="dat" -DartifactId="dcm4che" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn install:install-file -Dfile="./util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn install:install-file -Dfile="./log4j.jar" -DgroupId="dat" -DartifactId="log4j" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn install:install-file -Dfile="./pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn install:install-file -Dfile="./dcm4che-imageio-rle-2.0.25.jar" -DgroupId="dat" -DartifactId="dcm4che-imageio-rle-2.0.25" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn -ntp install:install-file -Dfile="./CTP.jar" -DgroupId="dat" -DartifactId="CTP" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn -ntp install:install-file -Dfile="./util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn -ntp install:install-file -Dfile="./pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
 
 # Install some required CTP dependencies
 mvn install:install-file -Dfile="./clibwrapper_jiio.jar" -DgroupId="dat" -DartifactId="clibwrapper_jiio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
