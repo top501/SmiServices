169c6a701183b593a8e621e18e3c190254bb8bbd
James A Sutherland <j@sutherland.pw>
2020-02-11 14:44:20 +0000

    Remove obsolete jar files from old library handling

--- lib/java/installDat.sh
+++ lib/java/installDat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install DATs dependencies to your local repo
-mvn -ntp install:install-file -Dfile="./CTP.jar" -DgroupId="dat" -DartifactId="CTP" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn -ntp install:install-file -Dfile="./util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-mvn -ntp install:install-file -Dfile="./pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn install:install-file -Dfile="./CTP.jar" -DgroupId="dat" -DartifactId="CTP" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn install:install-file -Dfile="./util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+mvn install:install-file -Dfile="./pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
 
 # Install some required CTP dependencies
 mvn install:install-file -Dfile="./clibwrapper_jiio.jar" -DgroupId="dat" -DartifactId="clibwrapper_jiio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
