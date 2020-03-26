02557ff4d46ffdb34f7d1df27fc3955619882dd5
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-03-26 18:47:39 +0000

    Add stub for TestRabbitMqAdapter

---
+++ tests/common/Smi.Common.Tests/TestRabbitMqAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using Smi.Common.Messaging;
+using Smi.Common.Options;
+
+namespace Smi.Common.Tests
+{
+    public class TestRabbitMqAdapter : IRabbitMqAdapter
+    {
+        public bool HasConsumers { get; }
+
+        public Guid StartConsumer(ConsumerOptions consumerOptions, IConsumer consumer, bool isSolo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopConsumer(Guid taskId, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProducerModel SetupProducer(ProducerOptions producerOptions, bool isBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IModel GetModel(string connectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown(TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
