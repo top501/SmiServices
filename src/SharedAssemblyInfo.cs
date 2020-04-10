bbd864af414bcdf2661c2c46404767a07e06a8e3
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-04-10 19:36:57 +0100

    Update SharedAssemblyInfo and add version file

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,11 +2,4 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Universities of Dundee and Edinburgh")]
-[assembly: AssemblyCopyright("Copyright © SMI Project 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// These should be overwritten by release builds
-[assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyFileVersion("1.7.0")]
-[assembly: AssemblyInformationalVersion("1.7.0")] // This one can have the extra build info after it
+[assembly: AssemblyCopyright("Copyright © SMI Project 2020")]
